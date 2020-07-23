b7d4580a4eaeb7fe04134bbb5cb11d087e8a1dad
Spandan Pal <2spandan2@gmail.com>
2020-07-23 12:59:54 +0530

    adding support for audio questions

--- inquest/settings.py
+++ inquest/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup.apps.CleanupConfig', # to delete media files after deleting from admin panel
     # 'social_django' ,
 ]
 
